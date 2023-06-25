2916f3459ad2ed55cd990f555afc6a523849673a
StealthWorm <thierrypitela@hotmail.com>
2023-06-24 21:56:46 -0300

    sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,8 +6,8 @@ import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
-// import * as Sentry from "@sentry/node";
-// import * as Tracing from "@sentry/tracing";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
@@ -19,17 +19,17 @@ const app = express();
 
 app.use(rateLimiter);
 
-// Sentry.init({
-//   dsn: process.env.SENTRY_DSN,
-//   integrations: [
-//     new Sentry.Integrations.Http({ tracing: true }),
-//     new Tracing.Integrations.Express({ app }),
-//   ],
-//   tracesSampleRate: 1.0,
-// });
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
-// app.use(Sentry.Handlers.requestHandler());
-// app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -41,7 +41,7 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 app.use(router);
 
-// app.use(Sentry.Handlers.errorHandler());
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
